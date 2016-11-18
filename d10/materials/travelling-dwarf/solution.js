2d42dfeb799f166a2b43ce300c5f7a2d9080d231
achansler <aachansler@gmail.com>
2016-11-17 21:32:39 -0500

    d10

---
+++ d10/materials/travelling-dwarf/solution.js
@@ -0,0 +1,69 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+
+
+var g = 0;
+
+while (g < 4 ) {
+	d.move();
+	g++;
+}
+
+d.orient('down');
+
+var g = 0;
+
+while (g < 16 ) {
+	d.move();
+	g++;
+}
+
+d.orient('right');
+
+var g = 0;
+
+while (g < 6 ) {
+	d.move();
+	g++;
+}
+
+d.orient('up');
+
+var g = 0;
+
+while (g < 11) {
+	d.move();
+	g++;
+}
+
+d.orient('right');
+
+var g = 0;
+
+while (g < 6) {
+	d.move();
+	g++;
+ }
+
+d.orient('down');
+
+var g = 0;
+
+while (g < 9) {
+	d.move();
+	g++;
+}
+
+d.orient('right');
+
+var g = 0;
+
+while (g < 2) {
+	d.move();
+	g++;
+}
+
+
