7733a88d8ba2e76ef848769f578216f6eddd3a01
achansler <abigailchansler@Abigails-MBP-2.home>
2016-12-12 21:40:55 -0500

    final project

---
+++ d14/materials/carousel/js/project.js
@@ -0,0 +1,9 @@
+// $("button#right").on("click", function() {
+// 	var image = $("img").val();
+// 	console.log(image);
+// });
+
+$("button#right").on("click", function() {
+	
+});
+
---
+++ final project/js/app.js
@@ -0,0 +1,21 @@
+
+$("button#no").on("click", function() {
+	$("h1.big").text("WRONG");
+	$("button#try-again").removeClass("hide");
+	$("button#yes").addClass("hide");
+	$("button#no").addClass("hide");
+});
+
+$("button#try-again").on("click", function() {
+	$("h1.big").text("Are sandwiches your favorite food?");
+	$("button#yes").removeClass("hide");
+	$("button#no").removeClass("hide");
+	$("button#try-again").addClass("hide");
+});
+
+$("button#yes").on("click", function() {
+	$("h1.big").text("Good Choice!");
+	$("button#make").removeClass("hide");
+	$("button#yes").addClass("hide");
+	$("button#no").addClass("hide");
+});
