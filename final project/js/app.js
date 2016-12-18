5f0e9fec6e3618fd3ec804752ae010b340772262
achansler <abigailchansler@Abigails-MBP-2.home>
2016-12-18 18:12:46 -0500

    updates on images

--- final project/js/app.js
+++ final project/js/app.js
@@ -47,4 +47,69 @@ $('.center').slick({
     }
   ]
 });
-        
+
+$("#one").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-01.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#two").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-02.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#three").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-04.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#four").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-05.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#five").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-06.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#six").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-03.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#seven").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-07.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#eight").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-08.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#nine").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-09.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#ten").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-10.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
+
+$("#eleven").on("click", function() {
+  $("img#big").attr("src", "img/sandwich ingredients-11.png");
+  $("button#next").removeClass("hide");
+  $("h2#empty").addClass("hide");
+});
