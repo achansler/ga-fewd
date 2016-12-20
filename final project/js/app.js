efed169a13ebe44c96ee560b7e3efd6ea3c9fe86
achansler <abigailchansler@Abigails-MBP-2.home>
2016-12-19 22:24:36 -0500

    cleaned up JQuery

--- final project/js/app.js
+++ final project/js/app.js
@@ -48,68 +48,168 @@ $('.center').slick({
   ]
 });
 
-$("#one").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-01.png");
+//Ingredients
+
+var ingredients = [
+{
+  photo: "img/sandwich ingredients-01.png"
+},
+{
+  photo: "img/sandwich ingredients-02.png"
+},
+{
+  photo: "img/sandwich ingredients-04.png"
+},
+{
+  photo: "img/sandwich ingredients-05.png"
+},
+{
+  photo: "img/sandwich ingredients-06.png"
+},
+{
+  photo: "img/sandwich ingredients-03.png"
+},
+{
+  photo: "img/sandwich ingredients-07.png"
+},
+{
+  photo: "img/sandwich ingredients-08.png"
+},
+{
+  photo: "img/sandwich ingredients-09.png"
+},
+{
+  photo: "img/sandwich ingredients-10.png"
+},
+{
+  photo: "img/sandwich ingredients-11.png"
+},
+{
+  photo: "img/sandwich ingredients-12.png"
+},
+{
+  photo: "img/sandwich ingredients-13.png"
+},
+{
+  photo: "img/sandwich ingredients-14.png"
+},
+{
+  photo: "img/sandwich ingredients-15.png"
+},
+{
+  photo: "img/sandwich ingredients-16.png"
+},
+{
+  photo: "img/sandwich ingredients-17.png"
+},
+{
+  photo: "img/sandwich ingredients-18.png"
+},
+{
+  photo: "img/sandwich ingredients-19.png"
+},
+{
+  photo: "img/sandwich ingredients-20.png"
+},
+{
+  photo: "img/sandwich ingredients-21.png"
+},
+{
+  photo: "img/sandwich ingredients-22.png"
+},
+];
+
+var changeImage = function(image) {
+  $("img#big").attr("src", ingredients[image]["photo"]);
   $("button#next").removeClass("hide");
   $("h2#empty").addClass("hide");
+}
+
+$("img#one").on("click", function() {
+  changeImage(0);
 });
 
-$("#two").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-02.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#two").on("click", function() {
+  changeImage(1);
 });
 
-$("#three").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-04.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#three").on("click", function() {
+  changeImage(2);
 });
 
-$("#four").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-05.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#four").on("click", function() {
+  changeImage(3);
 });
 
-$("#five").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-06.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#five").on("click", function() {
+  changeImage(4);
 });
 
-$("#six").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-03.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#six").on("click", function() {
+  changeImage(5);
 });
 
-$("#seven").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-07.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#seven").on("click", function() {
+  changeImage(6)
 });
 
-$("#eight").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-08.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#eight").on("click", function() {
+  changeImage(7)
 });
 
-$("#nine").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-09.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#nine").on("click", function() {
+  changeImage(8)
 });
 
-$("#ten").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-10.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#ten").on("click", function() {
+  changeImage(9)
 });
 
-$("#eleven").on("click", function() {
-  $("img#big").attr("src", "img/sandwich ingredients-11.png");
-  $("button#next").removeClass("hide");
-  $("h2#empty").addClass("hide");
+$("img#eleven").on("click", function() {
+  changeImage(10)
 });
+
+$("img#tweleve").on("click", function() {
+  changeImage(11)
+});
+
+$("img#thirteen").on("click", function() {
+  changeImage(12)
+});
+
+$("img#fourteen").on("click", function() {
+  changeImage(13)
+});
+
+$("img#fifteen").on("click", function() {
+  changeImage(14)
+});
+
+$("img#sixteen").on("click", function() {
+  changeImage(15)
+});
+
+$("img#seventeen").on("click", function() {
+  changeImage(16)
+});
+
+$("img#eighteen").on("click", function() {
+  changeImage(17)
+});
+
+$("img#nineteen").on("click", function() {
+  changeImage(18)
+});
+
+$("img#twenty").on("click", function() {
+  changeImage(19)
+});
+
+$("img#twenty-one").on("click", function() {
+  changeImage(20)
+});
+
+$("img#twenty-two").on("click", function() {
+  changeImage(21)
+});
+
